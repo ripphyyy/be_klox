f2d62402b0afacb5383dd28ee59c86168d54a4ee
Ripki-Dev <92913498+Ripki-Dev@users.noreply.github.com>
2022-06-29 08:24:00 +0700

    db fix

--- database/migrations/2021_01_17_161413_create_products_table.php
+++ database/migrations/2021_01_17_161413_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->text('profile_photo_path')->nullable();
+            $table->float('price');
+            $table->longText('description');
+
+            $table->string('tags')->nullable();
+
+            $table->bigInteger('categories_id');
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -33,6 +36,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
