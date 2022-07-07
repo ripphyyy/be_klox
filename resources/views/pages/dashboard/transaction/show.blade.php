abc50e8e5732d35235df69b02357ba591cbee36a
kurniaagung29 <kurniaagungzzz23@gmail.com>
2022-07-07 15:47:54 +0700

    agung fix product

--- resources/views/pages/dashboard/transaction/show.blade.php
+++ resources/views/pages/dashboard/transaction/show.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        
             Transaction &raquo; #{{ $transaction->id }} {{ $transaction->name }}
         </h2>
     </x-slot>
